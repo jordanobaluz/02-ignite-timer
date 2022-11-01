ffd90bae024e5246386cc15040699f4a2fa37b2a
JordanoBaluz <jordanolb95@hotmail.com>
2022-10-31 21:50:58 -0300

    definido a tipagem do tema, para aparecer no autocomplete

---
+++ src/@types/styles.d.ts
@@ -0,0 +1,11 @@
+/*.d.ts define que só terá código de definição de tipo ts, não pode ter código js*/
+import 'styled-components';
+import { defaultTheme } from '../styles/themes/default'
+
+type ThemeType = typeof defaultTheme;
+
+declare module 'styled-components' {
+    export interface DefaultTheme extends ThemeType {}
+}
+
+/* assim é modificado a tipagem de temas para poder ter uma melhor integração do TypeScript */
