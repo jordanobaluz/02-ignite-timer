683d0ef9288ea83537f4365455df46911b6102c2
JordanoBaluz <jordanolb95@hotmail.com>
2022-10-31 21:53:30 -0300

    criação de estilo global para ser utilizado no projeto

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,6 @@
 import { ThemeProvider } from 'styled-components'
 import { Button } from './components/Button'
+import { GlobalStyle } from './styles/global'
 import { defaultTheme } from './styles/themes/default'
 
 export function App() {
@@ -11,6 +12,9 @@ export function App() {
       <Button variant='danger' />
       <Button variant='success' />
       <Button />
+
+      Hello World
+      <GlobalStyle />
     </ThemeProvider>
   )
 }
