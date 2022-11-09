67bebdad92baf76332c4546f03c6014fffd11c93
JordanoBaluz <jordanolb95@hotmail.com>
2022-11-08 21:51:16 -0300

    criado metodo para armazenar data de quando time é interrompido, criado botão interromper e alterado forma de exibir o botão para começar e interromper

--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -1,4 +1,4 @@
-import { Play } from 'phosphor-react'
+import { HandPalm, Play } from 'phosphor-react'
 import { useForm } from 'react-hook-form'
 import { zodResolver } from '@hookform/resolvers/zod'
 import * as zod from 'zod'
@@ -10,6 +10,7 @@ import {
   MinutesAmoutInput,
   Separator,
   StartCountdownButton,
+  StopCountdownButton,
   TaskInput,
 } from './styles'
 import { useEffect, useState } from 'react'
@@ -42,6 +43,7 @@ interface Cycle {
   task: string
   minutesAmout: number
   startDate: Date // armazena a data que foi ativo para saber quanto tempo passou
+  interruptedDate?: Date // armazena data que foi interrompido manualmente o ciclo
 }
 
 export function Home() {
@@ -103,6 +105,20 @@ export function Home() {
     }
   }, [activeCycle])
 
+  function handleInterruptCycle() {
+    // anota dentro do ciclo se foi interrompido, para ter no historico
+    setCycles(
+      cycles.map((cycle) => {
+        if (cycle.id === activeCycleId) {
+          return { ...cycle, interruptedDate: new Date() }
+        } else {
+          return cycle
+        }
+      }),
+    )
+    setActiveCycleId(null)
+  }
+
   const totalSeconds = activeCycle ? activeCycle.minutesAmout * 60 : 0
   const currentSeconds = activeCycle ? totalSeconds - amountSecondsPassed : 0
   // armazena os minutos e segundos que restam
@@ -124,6 +140,8 @@ export function Home() {
   const task = watch('task')
   const isSubmitDisabled = !task
 
+  console.log(cycles)
+
   return (
     <HomeContainer>
       <form onSubmit={handleSubmit(handleCreateNewCycle)} action="">
@@ -133,6 +151,7 @@ export function Home() {
             id="task"
             list="task-suggestions"
             placeholder="Dê um nome para o seu projeto"
+            disabled={!!activeCycle} // !! converte para boolean
             {...register('task')}
           />
           {/* uso de spread ... para descontruir as funções e passar como propriedades do componente */}
@@ -151,6 +170,7 @@ export function Home() {
             step={5} /* pula de 5 em 5 ao clicar */
             min={5}
             max={60}
+            disabled={!!activeCycle} // !! converte para boolean
             {...register('minutesAmount', { valueAsNumber: true })}
           />
           <span>minutos.</span>
@@ -164,10 +184,17 @@ export function Home() {
           <span>{seconds[1]}</span>
         </CountdownContainer>
 
-        <StartCountdownButton disabled={isSubmitDisabled} type="submit">
-          <Play size={24} />
-          Começar
-        </StartCountdownButton>
+        {activeCycle ? (
+          <StopCountdownButton onClick={handleInterruptCycle} type="button">
+            <HandPalm size={24} />
+            Interromper
+          </StopCountdownButton>
+        ) : (
+          <StartCountdownButton disabled={isSubmitDisabled} type="submit">
+            <Play size={24} />
+            Começar
+          </StartCountdownButton>
+        )}
       </form>
       {/* form por volta de tudo para o button poder enviar as informações */}
     </HomeContainer>
