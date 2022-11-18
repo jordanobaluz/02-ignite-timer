6d7a0d78bb2ebfd5f9f86db5878116bcc1e7a344
JordanoBaluz <jordanolb95@hotmail.com>
2022-11-17 21:54:41 -0300

    Colocado o contexto no App pois é onde está o Router que faz a comunicação entre as paginas, assim permitindo a passagem por contexto das informações

--- src/App.tsx
+++ src/App.tsx
@@ -3,13 +3,17 @@ import { GlobalStyle } from './styles/global'
 import { defaultTheme } from './styles/themes/default'
 import { BrowserRouter } from 'react-router-dom'
 import { Router } from './Router'
+import { CyclesContextProvider } from './context/CyclesContext'
 
 export function App() {
   return (
     <ThemeProvider theme={defaultTheme}>
       {/* BrowserRouter precisa ficar por fora para poder trabalhar as rotas */}
       <BrowserRouter>
-        <Router />
+        {/* Colocar o provider do contexto no App, quando precisa acessar as informações nas diferentes pages */}
+        <CyclesContextProvider>
+          <Router />
+        </CyclesContextProvider>
       </BrowserRouter>
       <GlobalStyle />
     </ThemeProvider>
